dd63ca9e6b3aeecf493b49f7e6bf1ccc4601a6cf
pughal <pughalraj77@gmail.com>
2025-03-10 14:50:26 +0800

    Able to read table in pptx

--- read_functions.py
+++ read_functions.py
@@ -3,11 +3,13 @@ Read functions for diff types. For visualisation of data we are to use matplot l
 '''
 import pandas as pd
 import tabula
+from pptx import Presentation
 '''
     This class represents the unified data structure I am to return
     Upon looking at the datasets, dataset 1 is about 2 sport and fitness companies
     dataset 3 is about quarterly revenue n stuff
-    datset 4 is
+    datset 4 represents the summary of performance
+    for data visualisation, I think we could go about verifying the summaries for fitpro
     The unified data structure although it has 
 '''
 class Unified_data_structure:
@@ -21,6 +23,7 @@ class Unified_data_structure:
         self.read_json("datasets/dataset1.json")
         self.read_csv("datasets/dataset2.csv")
         self.read_pdf("datasets/dataset3.pdf")
+        self.read_pptx("datasets/dataset4.pptx")
 
     def read_json(self, filename) -> None:
         # data might be nested
@@ -38,12 +41,28 @@ class Unified_data_structure:
         # from checking what dfs returns, I see that it is a list with a single element
         # where dfs[0] is a pandas dataframe
         self.data['dataset_3'] = dfs[0]
-
         return None
     def read_pptx(self, filename) -> None:
         # convert to pdf then read pdf
         # will convert all the pptx files to pdf in specified dir
-        return None
+        # in this dataset we have a table in slide 2
+        pptx = Presentation(filename)
+        # after exploring the pptx object in python shell i found where table in slide 2 is stored
+        table = pptx.slides[1].shapes[1].table
+        # convert this table to a pandas dataframe
+        data = []
+        for row in table.rows:
+            row_data = [cell.text.strip() for cell in row.cells]
+            data.append(row_data)
+        
+        # Convert to Pandas DataFrame
+        headers = data[0]  # Assume first row contains headers
+        rows = data[1:]  # Remaining rows are data
+        df = pd.DataFrame(rows, columns=headers)
+        
+        # as for the information in slide 1 and 3, I think its best if we manually keyed in the information
+        self.data['dataset_4'] = df
+        
     def unify(self, data):
         # take existing data and unify it with current
         return None
